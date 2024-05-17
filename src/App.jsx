6ba94d3b90f373b9ec5d34def4a12d1a31805df9
Romero Armando <romeroarmando094@gmail.com>
2024-05-16 22:19:13 -0300

    xommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import Navbar from './components/navbar/Navbar'
 
 import './App.css'
+
 
 function App() {
 
   return (
     <> 
+  
       <div>
+     
+        <Navbar />
+      
         <h1>
           Local de Informatica
         </h1>
-      </div>      
+      </div>  
+
     </>
   )
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import img from '/assets/icono.png'
+
+import './Navbar.css'
+
+const Navbar = () => {
+
+    return (
+    
+        <div>
+
+            <div>
+
+                <img src={img} alt="local kiki" />
+
+            </div>
+
+        </div>
+    
+    )
+
+}
+
+export default Navbar
